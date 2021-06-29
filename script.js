aadab48b89615fd4cc5a93c91659523528b27fbb
Rogue <jacquesinnocentadv@gmail.com>
2021-06-29 15:29:18 -0700

    Added basic functionalities

---
+++ script.js
@@ -0,0 +1,22 @@
+setInterval(setClock, 1000)
+
+const hourStick = document.querySelector(['data-hour-stick'])
+const minuteStick = document.querySelector(['data-minute-stick'])
+const secondStick = document.querySelector(['data-second-hand'])
+
+function setClock() {
+    const currentDate = new Date()
+    const secondsRatio = currentDate.getSeconds() / 60
+    const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
+    const hoursRatio = (minutesRatio + currentDate.getHours()) / 12
+
+    setRotation(secondStick, secondsRatio)
+    setRotation(minuteStick, minutesRatio)
+    setRotation(hourStick, hoursRatio)
+}
+
+function setRotation(element, rotationRatio) {
+    element.style.setProperty('--rotation', rotationRatio * 360)
+}
+
+setClock()
